fb37a1d8b29918bec4d43b588681da76d3fd41fe
luigi_serra91@hotmail.it <luigi_serra91@hotmail.it>
2024-10-11 14:11:15 +0200

    snak_1

---
+++ main.js
@@ -0,0 +1,41 @@
+console.log('its works');
+
+
+/* 
+Snack 1
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore.
+*/
+const printBike = document.getElementById('bike')
+
+const bike = [
+    {
+        name : 'yellow bike',
+        weight : 60,
+        
+    },
+    {
+        name : 'Red bike',
+        weight : 100,
+        
+    },
+    {
+        name : 'pink bike',
+        weight : 70,
+        
+    }
+]
+
+ // Funzione per trovare la bici con il peso minore
+ const biciLeggera = bike.reduce((min, bike) => bike.weight < min.weight ? bike : min);
+
+ 
+ printBike.innerHTML = `${biciLeggera.weight} kg</strong>.`;
+
+console.log(bike);
+
+
+       
+        
+ 
